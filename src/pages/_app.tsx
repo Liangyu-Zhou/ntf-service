5c87092baf766f84431e1047e56b3aa2b03b237c
Liangyu-Zhou <17816872833@163.com>
2022-11-05 22:21:35 +0800

    resolve dependency problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout";
-import "../styles/globals.css";
+import "../styles/global.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
